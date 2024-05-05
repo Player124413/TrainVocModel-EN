fcfa90b05325ccbcacf2ec50d803668d0546d602
Player124413 <94734830+Player124413@users.noreply.github.com>
2024-05-05 19:05:00 +0300

    Update download_files.py

--- download_files.py
+++ download_files.py
@@ -6,7 +6,7 @@ os.makedirs(assets_folder, exist_ok=True)
 
 file_links = {
     "rmvpe/rmvpe.pt": "https://huggingface.co/Rejekts/project/resolve/main/rmvpe.pt",
-    "hubert/hubert_base.pt": "https://huggingface.co/Rejekts/project/resolve/main/hubert_base.pt"
+    "hubert/hubert_base.pt": "https://huggingface.co/spaces/Hobis/bark-voice-cloning-polish-HuBERT-quantizer/resolve/main/hubert.pt"
 }
 
 for file, link in file_links.items():
