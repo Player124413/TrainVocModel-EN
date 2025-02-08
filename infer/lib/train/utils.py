25305841da761c7bd4d2e16b93930bc2f3ae807a
Player1444 <94734830+Player124413@users.noreply.github.com>
2025-02-08 14:46:32 +0300

    Update utils.py

--- infer/lib/train/utils.py
+++ infer/lib/train/utils.py
@@ -235,8 +235,8 @@ def plot_alignment_to_numpy(alignment, info=None):
 
 
 def load_wav_to_torch(full_path):
-    sampling_rate, data = read(full_path)
-    return torch.FloatTensor(data.astype(np.float32)), sampling_rate
+    sample_rate, data = read(full_path)
+    return torch.FloatTensor(data.astype(np.float32)), sample_rate
 
 
 def load_filepaths_and_text(filename, split="|"):
