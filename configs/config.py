261f55bcea1958ee8cf69a9545fe1caee33093fd
Politrees <143968312+Bebra777228@users.noreply.github.com>
2025-02-20 22:49:55 +0500

    Update config.py

--- configs/config.py
+++ configs/config.py
@@ -7,24 +7,13 @@ from multiprocessing import cpu_count
 
 import torch
 
-try:
-    import intel_extension_for_pytorch as ipex  # pylint: disable=import-error, unused-import
-
-    if torch.xpu.is_available():
-        from infer.modules.ipex import ipex_init
-
-        ipex_init()
-except Exception:  # pylint: disable=broad-exception-caught
-    pass
 import logging
 
 logger = logging.getLogger(__name__)
 
 
 version_config_list = [
-    
     "v2/40k.json",
-    
     "v2/48k.json",
     "v2/32k.json",
 ]
