b740c4327c78372c430c7ff826a3ab8035c4c077
Player1444 <94734830+Player124413@users.noreply.github.com>
2025-04-20 00:01:42 +0300

    Update extract_feature-transformers.py

--- infer/modules/train/extract_feature-transformers.py
+++ infer/modules/train/extract_feature-transformers.py
@@ -64,8 +64,8 @@ class HubertModelWithFinalProj(HubertModel):
     def __init__(self, config):
         super().__init__(config)
         self.final_proj = nn.Linear(config.hidden_size, config.classifier_proj_size)
-models = HubertModelWithFinalProj.from_pretrained(model_path)
-return models
+model = HubertModelWithFinalProj.from_pretrained(model_path)
+model = model.to(device)
 if is_half and device not in ["mps", "cpu"]:
     model = model.half()
 model.eval()
