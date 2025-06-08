2ca679dc6a5f3e8eaaa0586aeed4774067159b6b
Player1444 <94734830+Player124413@users.noreply.github.com>
2025-06-08 12:40:58 +0300

    Update models.py

--- infer/lib/infer_pack/models.py
+++ infer/lib/infer_pack/models.py
@@ -637,7 +637,36 @@ sr2sr = {
     "48k": 48000,
 }
 
+class F0Decoder(nn.Module):
+    def __init__(self,
+                 out_channels,
+                 hidden_channels,
+                 filter_channels,
+                 n_heads,
+                 n_layers,
+                 kernel_size,
+                 p_dropout,
+                 spk_channels=0):
+        super().__init__()
+        self.out_channels = out_channels
+        self.hidden_channels = hidden_channels
+        self.filter_channels = filter_channels
+        self.n_heads = n_heads
+        self.n_layers = n_layers
+        self.kernel_size = kernel_size
+        self.p_dropout = p_dropout
+        self.spk_channels = spk_channels
 
+        self.prenet = nn.Conv1d(hidden_channels, hidden_channels, 3, padding=1)
+        self.decoder = attentions.FFT(
+            hidden_channels,
+            filter_channels,
+            n_heads,
+            n_layers,
+            kernel_size,
+            p_dropout)
+        self.proj = nn.Conv1d(hidden_channels, out_channels, 1)
+        self.f0_prenet = nn.Conv1d(1, hidden_channels, 3, paddi
 class SynthesizerTrnMs256NSFsid(nn.Module):
     def __init__(
         self,
